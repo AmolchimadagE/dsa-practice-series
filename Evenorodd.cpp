4a2f86e8bf698ba8286b268e0f736ad6b506f3ee
AmolchimadagE <131466503+AmolchimadagE@users.noreply.github.com>
2024-02-04 11:24:24 +0530

    Even or odd number

---
+++ Evenorodd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace  std;
+int main(){
+
+    int number ;
+    cout<<"Enter a number :";
+    cin>>number;
+    
+    //checking weather the number is even or odd
+
+    if(number % 2==0)
+    cout<<number<<":Even";
+ else 
+ cout<<number<<":odd";
+ return 0;
+ 
+
+
+}
