8553ead0b984105482a51160ef4b8f1b08cd2e12
AmolchimadagE <131466503+AmolchimadagE@users.noreply.github.com>
2024-02-04 11:56:04 +0530

    greater number three

---
+++ greatestthree.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+
+int first,second,third;
+first=10,second=20,third-30;
+
+//comparing first with other  numbers
+
+if((first>=second) && (first>=third)){
+cout<<first<<"is the greatet";
+}
+
+//comparing second with other numbers
+
+else if ((second>=first)&&(second>=third)) 
+cout<<second<<"is the greatest";
+// comparing third with other number
+else
+cout << "is the greatest"<< endl;
+return 0;
+
+
+}
+
