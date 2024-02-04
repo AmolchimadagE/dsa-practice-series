9d950d7e7b052189105755e5d1da1afde6cd9fbd
AmolchimadagE <131466503+AmolchimadagE@users.noreply.github.com>
2024-02-04 11:39:54 +0530

    greater number

---
+++ greaternumber.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std ;
+int main(){
+    int num1,num2;
+    num1=75,num2=85;
+
+    if(num1==num1)
+    cout<<"both are equal";
+    else if(num1>num2)
+    cout<<"is greater than"<<num2;
+    else
+    cout<<num1<<"is gretaer then"<<num1;
+    return 0;
+    
+}
