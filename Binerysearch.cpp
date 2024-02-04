d7a56fe004a6d03d2882ea9b36447e640c07c7b2
AmolchimadagE <131466503+AmolchimadagE@users.noreply.github.com>
2024-02-04 11:18:04 +0530

    positive or negative number

---
+++ Binerysearch.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarysearch(int array[], int left, int right, int item){
+    if(right>=left){
+
+        // calculation of new mid
+        int mid = left+(right - left)/2;
+
+        // return position where found 
+
+        if(array[mid] == item)
+        return mid+1;
+
+        // goes to recursive call in left half 
+        if()
+
+    }
+}
---
+++ positiveneg.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num =96;
+    //condition to check if the number is negative or positive
+
+    if(num>0)
+    cout<<"this number is positive";
+     else if(num<0)
+     cout<<"this number is negative";
+     else 
+     cout<<"zero";
+     
+}
